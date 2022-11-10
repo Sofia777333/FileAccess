280e44caaf2a4f6e9ed9908076305b383b5e3530
Sofia <117852305+Sofia777333@users.noreply.github.com>
2022-11-10 12:21:26 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace FilessAccess
+
+{
+    class Example
+    {
+     static void Main()
+        {
+            string path = @"C:\Users\User\Desktop\Директория";
+
+            if (Directory.Exists(path))
+            {
+                Console.ForegroundColor=ConsoleColor.Blue;
+                Console.WriteLine("Подкаталоги");
+            }
+            string[] directories = Directory.GetDirectories(path);
+            foreach (string directory in directories)
+            Console.WriteLine(directory);
+
+            Console.WriteLine();
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Файлы");
+            
+
+            string[] files = Directory.GetFiles(path);
+                  foreach (string file in files)
+                Console.WriteLine(file); 
+
+
+        }
+    }
+    
+}
+
